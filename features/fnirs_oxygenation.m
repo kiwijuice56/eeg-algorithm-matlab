c30be670115bef3cae523262d6f4e53e3dd6e73e
Eric Alfaro <ealfaro@mit.edu>
2025-10-10 15:24:31 -0400

    Tweaks to fNIRS algorithm

--- features/fnirs_oxygenation.m
+++ features/fnirs_oxygenation.m
@@ -1,88 +1,78 @@
-% Read data
-fS = 64; % base rampling rate of optics in the Muse headband
-signals = read_from_json_file("data/eric_alfaro/breath_holding_1.json", fS);
+% Parameters
+fS = 10;               % Hz
+baseline_seconds = 20; % baseline duration for initial intensity calculation
+d = 2.8;               % source-detector distance [cm] for outer sep
+DPF = [5.5, 5.5];      % differential pathlength factors (730 nm, 850nm)
+epsilon = [400, 1500;  % 730 nm: [HbO, HbR], https://omlc.org/spectra/hemoglobin/moaveni.html
+           1060, 800]; % 850 nm: [HbO, HbR]
+epsilon_uM = epsilon * 1e-6;
+
+% Load signals
+signals = read_from_json_file("data/eric_alfaro/breath_holding_6.json", fS);
 
 input_marker = signals.keyboard_input0.value;
 
-% Raw units are in microamps 
+% Units are all in microamps;
+% Each signal is proportional to light intensity
+outer_left_730  = signals.optics0.value(:);
+outer_right_730 = signals.optics1.value(:);
+outer_left_850  = signals.optics2.value(:);
+outer_right_850 = signals.optics3.value(:);
 
-% 730nm
-left_outer = signals.optics0.value;
-right_outer = signals.optics1.value;
+inner_left_730  = signals.optics4.value(:);
+inner_right_730 = signals.optics5.value(:); 
+inner_left_850  = signals.optics6.value(:);
+inner_right_850 = signals.optics7.value(:); 
 
-% 850nm
-left_inner = signals.optics2.value;
-right_inner = signals.optics3.value;
+t = (0:length(outer_left_730)-1) / fS;
 
-% Band-pass filter and trim our signals 
-fpass = 1.5; % Hz
-trim = 30; % number of samples
+% Convert intensity to optical density (OD) using baseline average
+baseline_samples = round(baseline_seconds * fS);
+proc_channel = @(sig730, sig850) ...
+    ( ...
+    -log10([sig730 ./ mean(sig730(1:baseline_samples)), ...
+          sig850 ./ mean(sig850(1:baseline_samples))]) ...
+    );
 
-filtered_left_outer = lowpass(left_outer, fpass, fS);
-filtered_left_outer = filtered_left_outer(trim:end - trim);
+OD_left  = proc_channel(outer_left_730,  outer_left_850);
+OD_right = proc_channel(outer_right_730, outer_right_850);
 
-filtered_right_outer = lowpass(right_outer, fpass, fS);
-filtered_right_outer = filtered_right_outer(trim:end - trim);
 
-filtered_left_inner = lowpass(left_inner, fpass, fS);
-filtered_left_inner = filtered_left_inner(trim:end - trim);
+% Convert OD to delta OD
+deltaOD_left = OD_left - mean(OD_left(1:baseline_samples));
+deltaOD_right = OD_right - mean(OD_right(1:baseline_samples)); 
 
-filtered_right_inner = lowpass(right_inner, fpass, fS);
-filtered_right_inner = filtered_right_inner(trim:end - trim);
-
-input_marker = input_marker(trim:end - trim);
-
-% Calculate baseline intensity
-baseline_samples = fS * 5; % first 5 seconds
-I0_left = [mean(filtered_left_outer(1:baseline_samples)), ...
-           mean(filtered_left_inner(1:baseline_samples))];
-
-I0_right = [mean(filtered_right_outer(1:baseline_samples)), ...
-            mean(filtered_right_inner(1:baseline_samples))];
-
-OD_left = -log([filtered_left_outer ./ I0_left(1), ...
-                filtered_left_inner ./ I0_left(2)]);
-
-OD_right = -log([filtered_right_outer ./ I0_right(1), ...
-                 filtered_right_inner ./ I0_right(2)]);
-
-% Extinction coefficients
-epsilon = [390,   1102.2;   % 730 nm
-           1058,  691.32];  % 850 nm
-
-d = 3.0;            % cm (measurea actual later)
-DPF = [5.5, 5.0];   % refine based on user
-scale = d .* DPF;
-
-E = [epsilon(1,:)*scale(1)*1e-6;
-     epsilon(2,:)*scale(2)*1e-6]; % scale to µM
+% Apply MBLL
+scale = d .* DPF; 
+E = epsilon_uM .* scale;
 pinvE = pinv(E);
 
-DeltaC_left  = (pinvE * OD_left')';   % N x 2
-DeltaC_right = (pinvE * OD_right')';
+C_left  = (pinvE * deltaOD_left')';   % delta[HbO, HbR] for left outer
+C_right = (pinvE * deltaOD_right')';  % delta[HbO, HbR] for right outer
 
-HbO_left = DeltaC_left(:,1);
-HbR_left = DeltaC_left(:,2);
+HbO_left  = C_left(:,1);  HbR_left  = C_left(:,2);
+HbO_right = C_right(:,1); HbR_right = C_right(:,2);
 
-HbO_right = DeltaC_right(:,1);
-HbR_right = DeltaC_right(:,2);
-
-t = (0:length(HbO_left)-1)/fS;
-
+% Plots
 figure;
 subplot(3,1,1);
-plot(t, HbO_left, 'r', t, HbR_left, 'b');
-xlabel('Time (s)'); ylabel('\Delta [Hb] (\muM)');
-title('Left channel'); legend('HbO','HbR');
+plot(t, HbO_left, 'r', 'LineWidth',1.2); hold on;
+plot(t, HbR_left, 'b', 'LineWidth',1.2);
+xlabel('Time (s)'); ylabel('\Delta[Hb] (\muM)');
+legend('HbO_2','HbR');
+title('Outer Left');
 
 subplot(3,1,2);
-plot(t, HbO_right, 'r', t, HbR_right, 'b');
-xlabel('Time (s)'); ylabel('\Delta [Hb] (\muM)');
-title('Right channel'); legend('HbO','HbR');
+plot(t, HbO_right, 'r', 'LineWidth',1.2); hold on;
+plot(t, HbR_right, 'b', 'LineWidth',1.2);
+xlabel('Time (s)'); ylabel('\Delta[Hb] (\muM)');
+legend('HbO_2','HbR');
+title('Outer Right');
 
-% Trigger plot
 subplot(3,1,3);
-plot(t, input_marker, 'g');
+plot((0:length(input_marker)-1) / fS, input_marker, 'g');
 xlabel('Time (s)');
 ylabel('Trigger');
 title('Input Trigger');
+
+
