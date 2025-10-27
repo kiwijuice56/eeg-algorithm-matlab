4ffae289cb2ae157f9f7541e8a79c1de7eb9f9e2
Eric Alfaro <ealfaro@mit.edu>
2025-10-27 10:26:48 -0400

    Fixed plot bugs

--- test/test_read_from_json_trial.m
+++ test/test_read_from_json_trial.m
@@ -2,15 +2,14 @@ trials = read_from_json_file_raw_trial("data/erp/f38cedd5-55e8-4e0e-be2c-9aae00f
 
 Fs = 256;
 channel = 2;
-highpassFrequency = 1;
-lowpassFrequency = 30;
+highpassFrequency = 0.5;
+lowpassFrequency = 50;
 
-plottedTrials = [trials.related, trials.unrelated];
-
+plottedTrials = {trials.related, trials.unrelated};
 figure; hold on;
 
 for j = 1:length(plottedTrials)
-    trial = plottedTrials(j);
+    trial = plottedTrials{j};
     averageEeg = lowpass(highpass(trial{1}.eeg(:,channel), highpassFrequency, Fs), lowpassFrequency, Fs);
     for i = 2:length(trial)
         a = averageEeg;
@@ -23,10 +22,11 @@ for j = 1:length(plottedTrials)
         averageEeg = a + b;
     end
     
-    eeg = -averageEeg / length(trial);
+    eeg = averageEeg / length(trial);
     plot((1 : length(eeg)) / Fs, eeg);
 end
 
+set(gca, 'YDir','reverse')
 xline(1.0,'-w',{'Stimulus'}); % Hardcoded, stimulus was presented at t = 1.0
 xline(1.1,'--r',{'N1'}); 
 xline(1.2,'--g',{'P2'}); 
@@ -34,4 +34,4 @@ xline(1.4,'--r',{'N400'});
 legend('Related', 'Unrelated')
 xlabel('Time (s)');
 ylabel('Amplitude (microvolts)');
-title('ERP of word pair task (AF7)');
+title('ERP of word pair task (right ear)');
