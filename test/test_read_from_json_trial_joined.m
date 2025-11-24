eeb2f388ec2e25ca5cc4b7087b8ec8a80c2ad99e
Eric Alfaro <ealfaro@mit.edu>
2025-11-24 10:05:12 -0500

    Fixes to syncing

--- test/test_read_from_json_trial_joined.m
+++ test/test_read_from_json_trial_joined.m
@@ -5,8 +5,9 @@ Fs = 256;
 trialLength = 0.5 + 1.0;
 trialSampleCount = round(trialLength * Fs);
 
-[b_bp, a_bp] = butter(3, [0.5 30] / (Fs/2), 'bandpass');
+[b_bp, a_bp] = butter(3, [0.5 20] / (Fs/2), 'bandpass');
 eeg = filtfilt(b_bp, a_bp, data.notch_filtered_eeg.values(:, channel));
+eeg = detrend(eeg);
 
 averagedEeg = {[], []};
 averagedEegCount = {0, 0};
@@ -17,12 +18,13 @@ figure; hold on;
 % Skip some trials to avoid filtering issues
 skip = 3;
 for j = (skip + 1):length(data.trial_labels) - skip
-    startTime = data.start_times(j) - data.start_times(1);
-    startTimestamp = 1 + round(startTime * Fs);
+    stimulusTime = data.stimulus_times(j) - data.start_times(1);
+    stimulusTimestamp = 1 + round(stimulusTime * Fs);
+    startTimestamp = stimulusTimestamp - 128;
 
     window = eeg(startTimestamp : min(end, startTimestamp + trialSampleCount));
-    baseline = mean(window(1:round(0.2 * Fs))); % first 400 ms pre-stimulus
-    window = window - baseline;
+    % baseline = mean(window(1:round(0.3 * Fs))); % first 300 ms pre-stimulus
+    % window = window - baseline;
     
     label = data.trial_labels(j);
     if startsWith(label, "trial_related")
@@ -50,6 +52,9 @@ for i = 1:2
     plot((1 : length(averagedEeg{i})) / Fs, averagedEeg{i}, 'LineWidth', 1);
 end
 
+% Plot difference
+% plot((1 : length(averagedEeg{1})) / Fs, averagedEeg{2} - averagedEeg{1}, 'LineWidth', 1);
+
 
 set(gca, 'YDir','reverse')
 xline(0.5,'-k',{'Stimulus start (approx.)'}); 
@@ -57,4 +62,4 @@ xline(0.5,'-k',{'Stimulus start (approx.)'});
 legend('Normal', 'Strange')
 xlabel('Time (s)');
 ylabel('Amplitude (microvolts)');
-title('ERP of audio sentence task (right forehead)');
+title('ERP of oddball task (left forehead)');
