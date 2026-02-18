ccc8748332a3da63cd1aea433207b23e13c13789
Eric Alfaro <alfaroeric127@gmail.com>
2026-02-18 13:29:05 -0500

    Alpha peak

--- test/test_alpha_peak.m
+++ test/test_alpha_peak.m
@@ -1,24 +1,27 @@
 % Testing: Finding alpha peak from eyes closed data
 
 Fs = 256; 
-win = hanning(1024);
-noverlap = 512;
-nfft = 1024;
+win = hanning(512);
+noverlap = 256;
+nfft = 2048; 
 channel = 1;
 rereference = false;
 
 figure; hold on;
 
-trials = {'eyes_closed_4'};
+trials = {'eyes_closed_long'};
 labels = {'Eyes closed'};
 for trialnum = 1:length(trials) 
     trial = trials{trialnum};
 
-    signals = read_from_json_file_raw(sprintf("data/eric_alfaro/%s.json", trial), "notch_filtered_eeg");
-    signal = signals.notch_filtered_eeg.data(channel, :);
+    signals = read_from_json_file_raw(sprintf("data/pc/eric_alfaro/%s.json", trial), "eeg");
+    signal = signals.eeg.data(channel, :);
     signal = detrend(signal); 
-
+    
     [pxx, f] = pwelch(signal, win, noverlap, nfft, Fs);
+    
+    %pxx_smooth = movmean(pxx, 2);
+
     plot(f, pow2db(pxx), 'DisplayName', labels{trialnum}); 
 end
 
